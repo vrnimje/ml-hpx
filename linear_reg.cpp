846e5edeab7a3bb492e7f6a9c66bbe60cf13a930
Vedant <vedantnimjed@gmail.com>
2023-10-27 14:54:57 +0530

    Added code for linear regression
    Signed-off-by: Vedant <vedantnimjed@gmail.com>

---
+++ linear_reg.cpp
@@ -0,0 +1,102 @@
+#include <hpx/algorithm.hpp>
+#include <hpx/future.hpp>
+#include <hpx/init.hpp>
+#include <hpx/iostream.hpp>
+
+#include <iostream>
+#include <random>
+#include <vector>
+#include <fstream>
+
+float f(float X, float W, float B) {
+    return (W*X + B);
+}
+
+int hpx_main(int argc, char* argv[]) {
+    std::string filename;
+    if (argc > 1) {
+        filename = argv[1];
+    } else {
+        std::cerr << "Error: No input file provided." << std::endl;
+        return 1;
+    }
+    std::ifstream in(filename, std::ios_base::in);
+
+    if (!in) {
+        std::cerr << "Error: Could not open input file: " << filename << std::endl;
+        return 1;
+    }
+
+    std::vector<float> X, Y;
+    std::string line;
+
+    std::getline(in, line); // Skip column names
+    int l = 0;
+
+    while (std::getline(in, line)) {
+        std::stringstream L(line); 
+        std::string data;
+        while(getline(L, data, ',')) {
+            // std::cout << data << "\n";
+            l++;
+            if (l%2 != 0) {
+                X.push_back(std::stof(data));
+            }
+            else 
+                Y.push_back(std::stof(data));
+        }
+    }
+
+    // No. of data points
+    int n = X.size();
+
+    // for (int i=0; i<n; i++) {
+    //     std::cout << Y[i] << ", "; 
+    // }
+
+    // Linear equation: Y = WX + B
+
+    std::random_device rd;  // Will be used to obtain a seed for the random number engine
+    std::mt19937 gen(rd());
+
+    std::uniform_real_distribution<float> distribution(-1.0, 1.0);
+
+    float W = distribution(gen), B = distribution(gen);
+    float prev_W, prev_B;
+
+    int N = 10000;
+    float alpha = 0.015;
+
+    // Gradient Descent
+
+    for (int k=0; k<N; k++) {
+    
+        float dj_dw = 0, dj_db = 0;
+
+        for (int i=0; i<n; i++) {
+            dj_dw += (f(X[i], W, B) - Y[i]) * X[i];
+            dj_db += (f(X[i], W, B) - Y[i]);
+        }
+
+        dj_dw /= n;
+        dj_db /= n;
+
+        //std::cout << "Derivatives: " << dj_dw << "," << dj_db << "\n";
+
+        prev_W = W;
+        prev_B = B;
+
+        W -= alpha * dj_dw;
+        B -= alpha * dj_dw;
+
+        // if (prev_W == W && prev_B == B) break;
+    }
+
+    std::cout << "Final parameters: " << W << "," << B << "\n";
+
+    return hpx::local::finalize();
+}
+
+int main(int argc, char* argv[]) {
+    return hpx::local::init(hpx_main, argc, argv);
+}
