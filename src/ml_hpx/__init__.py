327402bddf8a0bb9afdfe621527c590530282b70
Vedant <vedantnimjed@gmail.com>
2025-10-20 23:49:15 +0530

    Trying exception handling & disabling CUDA packages
    Signed-off-by: Vedant <vedantnimjed@gmail.com>

--- src/ml_hpx/__init__.py
+++ src/ml_hpx/__init__.py
@@ -1,8 +1,29 @@
-from ._ml_hpx_impl import LinearRegression, LogisticRegression, KNearestNeighbours, KMeansClustering, Perceptron, SVC
+from ._ml_hpx_impl import (
+    LinearRegression,
+    LogisticRegression,
+    KNearestNeighbours,
+    KMeansClustering,
+    Perceptron,
+    SVC,
+)
 from ._ml_hpx_impl import finalize, initialize
 from ._ml_hpx_impl import NeuralNetwork, Layer, Optimizer, SGD
 
 import atexit
+
+
+def safe_initialize():
+    try:
+        initialize()
+    except OSError as e:
+        if "libnvidia" in str(e):
+            # Ignore missing GPU libraries on CPU-only systems
+            pass
+        else:
+            raise
+
+
+safe_initialize()
 
 initialize()
 
